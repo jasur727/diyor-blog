a64d8c1c95d66648ac44655a75edf59e872c47e2
javohir <javohirxan0211@gmail.com>
2021-10-14 04:03:16 -0700

    add blog change

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 var elHeaderSection = document.querySelector('.header');
 var elHeaderButton = elHeaderSection.querySelector('.button-togler');
 
-
 if(elHeaderButton){
   elHeaderButton.addEventListener('click', function() {
     elHeaderSection.classList.toggle('header-open');
